583a241c977559da8dbe67d1732b981268414af1
Henrique Lira Da Silva <henrique.2019210942@unicap.br>
2022-11-23 20:25:47 -0300

    +1

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.post("/loginPaciente", (req, res) => {
   );
 });
 
-app.listen(process.env.PORT || 3001, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
